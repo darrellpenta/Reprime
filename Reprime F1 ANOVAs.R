0abe080d692967068aff66b26123d9d48021467b
Darrell Penta <d.penta@neu.edu>
2015-03-09 11:09:16 -0400

    Reviewed to ask Neal

--- Reprime F1 ANOVAs.R
+++ Reprime F1 ANOVAs.R
@@ -1,245 +1,112 @@
 rm(list = ls()) # clears environment
 library(languageR) # calls languageR library
 
+## -----------------------------------PREPARE DATA FILE FOR ANALYSES---------------------------------
 #
-# -----------------------------------PREPARE DATA FILE FOR ANALYSES---------------------------------
+f1vtimeout <- read.table("data/vtimeout.txt", header = F) # reads in all data from data file
+colnames(f1vtimeout) <- c("subj", "item", "subexp", "headrel", "localrel", "vtime")
+d.cat <- subset(f1vtimeout, subexp == "Cat")
+d.cat$subj <- as.factor(d.cat$subj) # designates "subject" as a factor
+d.prop <- subset(f1vtimeout, subexp == "Attr")
+d.prop$subj <- as.factor(d.prop$subj) # designates "subject" as a factor
+d.semrel <- subset(f1vtimeout, subexp == "SemRel")
+d.semrel$subj <- as.factor(d.semrel$subj) # designates "subject" as a factor
+
+#========================================================================================================
 #
-f1errout <- read.table("data/SR_F1_errordata.txt", header = T) # reads in all data from data file
-
-d <- f1errout # renames data file
-
-d$subj <- as.factor(d$subj) # designates "subject" as a factor
-
-# Calculates the error rates (percent, including dys)
-d$pct <- ifelse(d$errd == 0 & d$errcord == 0, 0, (d$errd / (d$errcord)) * 100)  
-
-#aggregates d with dysfluencies 
-data.subj <- aggregate(d$pct, list(d$subj, d$semint, d$related, d$n2num ), mean) 
-
-colnames(data.subj) <- c("subj", "semint", "related", "n2num", "error") # renames columns
+#------------------------------------CATEGORY ITEMS--------------------------------------
+#
 
 # Below, designates various subsets of the original data file
-integ <- subset(data.subj, semint   ==  "integ") 
-unint <- subset(data.subj, semint   ==  "unint")
-relat <- subset(data.subj, related  ==  "rel") 
-unrel <- subset(data.subj, related  ==  "unrel") 
-sing  <- subset(data.subj, n2num    ==  "sing") 
-plur  <- subset(data.subj, n2num    ==  "plur")
+HeadN   <- subset(d.cat, headrel   ==  "HeadN") 
+UnrHead <- subset(d.cat, headrel   ==  "UnrHead")
+NRel    <- subset(d.cat, localrel  ==  "NRel") 
+NUnr    <- subset(d.cat, localrel  ==  "NUnr") 
 
-#Below, additional subsetted groups
-relat.int.plur   <- subset(data.subj, related == "rel"   & semint  == "integ" & n2num == "plur") 
-relat.int.sing   <- subset(data.subj, related == "rel"   & semint  == "integ" & n2num == "sing") 
-relat.unint.plur <- subset(data.subj, related == "rel"   & semint  == "unint" & n2num == "plur") 
-relat.unint.sing <- subset(data.subj, related == "rel"   & semint  == "unint" & n2num == "sing")
-unrel.int.plur   <- subset(data.subj, related == "unrel" & semint  == "integ" & n2num == "plur")
-unrel.int.sing   <- subset(data.subj, related == "unrel" & semint  == "integ" & n2num == "sing")
-unrel.unint.plur <- subset(data.subj, related == "unrel" & semint  == "unint" & n2num == "plur")
-unrel.unint.sing <- subset(data.subj, related == "unrel" & semint  == "unint" & n2num == "sing") 
-relat.plur       <- subset(data.subj, related == "rel"   & n2num   == "plur")
-relat.sing       <- subset(data.subj, related == "rel"   & n2num   == "sing")
-unrel.plur       <- subset(data.subj, related == "unrel" & n2num   == "plur")
-unrel.sing       <- subset(data.subj, related == "unrel" & n2num   == "sing")
-integ.plur       <- subset(data.subj, semint  == "integ" & n2num   == "plur") 
-integ.sing       <- subset(data.subj, semint  == "integ" & n2num   == "sing")
-unint.plur       <- subset(data.subj, semint  == "unint" & n2num   == "plur")
-unint.sing       <- subset(data.subj, semint  == "unint" & n2num   == "sing")
-integ.relat      <- subset(data.subj, semint  == "integ" & related == "rel") 
-integ.unrel      <- subset(data.subj, semint  == "integ" & related == "unrel")
-unint.relat      <- subset(data.subj, semint  == "unint" & related == "rel") 
-unint.unrel      <- subset(data.subj, semint  == "unint" & related == "unrel") 
-
+HeadN.Nrel <- subset(d.cat, headrel   ==  "HeadN" & localrel  ==  "NRel") 
+HeadN.NUnr <- subset(d.cat, headrel   ==  "HeadN" & localrel  ==  "NUnr") 
+UnrHead.Nrel <- subset(d.cat, headrel   ==  "UnrHead" & localrel  ==  "NRel") 
+UnrHead.NUnr <- subset(d.cat, headrel   ==  "UnrHead" & localrel  ==  "NUnr")
 
 ds <- data.frame(data = c(
   "gmean",
-  "integ",
-  "unint",
-  "relat",
-  "unrel",
-  "integrel",
-  "integunrel",
-  "unintrel",
-  "unintunrel",
-  "plur",
-  "sing",
-  "intplur",
-  "intsing",
-  "unintplur",
-  "unintsing",
-  "relplur",
-  "relsing",
-  "unrelplur",
-  "unrelsing",
-  "relintplur",
-  "relintsing",
-  "relunintplur",
-  "relunintsing",
-  "unrelintplur",
-  "unrelintsing",
-  "unrelunintplur",
-  "unrelunintsing"),
+  "HeadN",
+  "UnrHead",
+  "NRel",
+  "NUnr",
+  "HeadN-NRel",
+  "HeadN-NUnr",
+  "UnrHead-NRel",
+  "UnrHead-NUnr"),
   
-  n = c(length(data.subj$error),
-        length(integ$error),
-        length(unint$error),
-        length(relat$error),
-        length(unrel$error),
-        length(integ.relat$error),
-        length(integ.unrel$error),
-        length(unint.relat$error),
-        length(unint.unrel$error),
-        length(plur$error),
-        length(sing$error),
-        length(integ.plur$error),
-        length(integ.sing$error),
-        length(unint.plur$error),
-        length(unint.sing$error),
-        length(relat.plur$error),
-        length(relat.sing$error),
-        length(unrel.plur$error),
-        length(unrel.sing$error),
-        length(relat.int.plur$error),
-        length(relat.int.sing$error),
-        length(relat.unint.plur$error),
-        length(relat.unint.sing$error),
-        length(unrel.int.plur$error),
-        length(unrel.int.sing$error),
-        length(unrel.unint.plur$error),
-        length(unrel.unint.sing$error)),
+  n = c(length(d.cat$vtime),
+        length(HeadN$vtime), 
+        length(UnrHead$vtime),
+        length(NRel$vtime), 
+        length(NUnr$vtime), 
+        length(HeadN.Nrel$vtime),
+        length(HeadN.NUnr$vtime),
+        length(UnrHead.Nrel$vtime),
+        length(UnrHead.NUnr$vtime)),
   
-  N = c(length(data.subj$error),
-        length(integ$error),
-        length(unint$error),
-        length(relat$error),
-        length(unrel$error),
-        length(integ.relat$error),
-        length(integ.unrel$error),
-        length(unint.relat$error),
-        length(unint.unrel$error),
-        length(plur$error),
-        length(sing$error),
-        length(integ.plur$error),
-        length(integ.sing$error),
-        length(unint.plur$error),
-        length(unint.sing$error),
-        length(relat.plur$error),
-        length(relat.sing$error),
-        length(unrel.plur$error),
-        length(unrel.sing$error),
-        length(relat.int.plur$error),
-        length(relat.int.sing$error),
-        length(relat.unint.plur$error),
-        length(relat.unint.sing$error),
-        length(unrel.int.plur$error),
-        length(unrel.int.sing$error),
-        length(unrel.unint.plur$error),
-        length(unrel.unint.sing$error)),
+  N = c(length(d.cat$vtime),
+        length(HeadN$vtime), 
+        length(UnrHead$vtime),
+        length(NRel$vtime), 
+        length(NUnr$vtime), 
+        length(HeadN.Nrel$vtime),
+        length(HeadN.NUnr$vtime),
+        length(UnrHead.Nrel$vtime),
+        length(UnrHead.NUnr$vtime)),
   
-  mean = c(mean(data.subj$error),
-           mean(integ$error),
-           mean(unint$error),
-           mean(relat$error),
-           mean(unrel$error),
-           mean(integ.relat$error),
-           mean(integ.unrel$error),
-           mean(unint.relat$error),
-           mean(unint.unrel$error),
-           mean(plur$error),
-           mean(sing$error),
-           mean(integ.plur$error),
-           mean(integ.sing$error),
-           mean(unint.plur$error),
-           mean(unint.sing$error),
-           mean(relat.plur$error),
-           mean(relat.sing$error),
-           mean(unrel.plur$error),
-           mean(unrel.sing$error),
-           mean(relat.int.plur$error),
-           mean(relat.int.sing$error),
-           mean(relat.unint.plur$error),
-           mean(relat.unint.sing$error),
-           mean(unrel.int.plur$error),
-           mean(unrel.int.sing$error),
-           mean(unrel.unint.plur$error),
-           mean(unrel.unint.sing$error)),
+  mean = c(mean(d.cat$vtime),
+           mean(HeadN$vtime), 
+           mean(UnrHead$vtime),
+           mean(NRel$vtime), 
+           mean(NUnr$vtime), 
+           mean(HeadN.Nrel$vtime),
+           mean(HeadN.NUnr$vtime),
+           mean(UnrHead.Nrel$vtime),
+           mean(UnrHead.NUnr$vtime)),
   
-  sd = c(sd(data.subj$error),
-         sd(integ$error),
-         sd(unint$error),
-         sd(relat$error),
-         sd(unrel$error),
-         sd(integ.relat$error),
-         sd(integ.unrel$error),
-         sd(unint.relat$error),
-         sd(unint.unrel$error),
-         sd(plur$error),
-         sd(sing$error),
-         sd(integ.plur$error),
-         sd(integ.sing$error),
-         sd(unint.plur$error),
-         sd(unint.sing$error),
-         sd(relat.plur$error),
-         sd(relat.sing$error),
-         sd(unrel.plur$error),
-         sd(unrel.sing$error),
-         sd(relat.int.plur$error),
-         sd(relat.int.sing$error),
-         sd(relat.unint.plur$error),
-         sd(relat.unint.sing$error),
-         sd(unrel.int.plur$error),
-         sd(unrel.int.sing$error),
-         sd(unrel.unint.plur$error),
-         sd(unrel.unint.sing$error)),
+  sd = c(sd(d.cat$vtime),
+         sd(HeadN$vtime), 
+         sd(UnrHead$vtime),
+         sd(NRel$vtime), 
+         sd(NUnr$vtime), 
+         sd(HeadN.Nrel$vtime),
+         sd(HeadN.NUnr$vtime),
+         sd(UnrHead.Nrel$vtime),
+         sd(UnrHead.NUnr$vtime)),
   
-  se = c(sd(data.subj$error) / sqrt(length(data.subj$error)),
-         sd(integ$error) / sqrt(length(integ$error)),
-         sd(unint$error) / sqrt(length(unint$error)),
-         sd(relat$error) / sqrt(length(relat$error)),
-         sd(unrel$error) / sqrt(length(unrel$error)),
-         sd(integ.relat$error) / sqrt(length(integ.relat$error)),
-         sd(integ.unrel$error) / sqrt(length(integ.unrel$error)),
-         sd(unint.relat$error) / sqrt(length(unint.relat$error)),
-         sd(unint.unrel$error) / sqrt(length(unint.unrel$error)),
-         sd(plur$error) / sqrt(length(plur$error)),
-         sd(sing$error) / sqrt(length(sing$error)),
-         sd(integ.plur$error) / sqrt(length(integ.plur$error)),
-         sd(integ.sing$error) / sqrt(length(integ.sing$error)),
-         sd(unint.plur$error) / sqrt(length(unint.plur$error)),
-         sd(unint.sing$error) / sqrt(length(unint.sing$error)),
-         sd(relat.plur$error) / sqrt(length(relat.plur$error) ),
-         sd(relat.sing$error) / sqrt(length(relat.sing$error)),
-         sd(unrel.plur$error) / sqrt(length(unrel.plur$error)),
-         sd(unrel.sing$error) / sqrt(length(unrel.sing$error)),
-         sd(relat.int.plur$error) / sqrt(length(relat.int.plur$error)),
-         sd(relat.int.sing$error) / sqrt(length(relat.int.sing$error)),
-         sd(relat.unint.plur$error) / sqrt(length(relat.unint.plur$error)),
-         sd(relat.unint.sing$error) / sqrt(length(relat.unint.sing$error)),
-         sd(unrel.int.plur$error) / sqrt(length(unrel.int.plur$error)),
-         sd(unrel.int.sing$error) / sqrt(length(unrel.int.sing$error)),
-         sd(unrel.unint.plur$error) / sqrt(length(unrel.unint.plur$error)),
-         sd(unrel.unint.sing$error) / sqrt(length(unrel.unint.sing$error))
+  se = c(sd(d.cat$vtime)       / sqrt(length(d.cat$vtime)),
+         sd(HeadN$vtime)       / sqrt(length(HeadN$vtime)), 
+         sd(UnrHead$vtime)     / sqrt(length(UnrHead$vtime)),
+         sd(NRel$vtime)        / sqrt(length(NRel$vtime)), 
+         sd(NUnr$vtime)        / sqrt(length(NUnr$vtime)), 
+         sd(HeadN.Nrel$vtime)  / sqrt(length(HeadN.Nrel$vtime)),
+         sd(HeadN.NUnr$vtime)  / sqrt(length(HeadN.NUnr$vtime)),
+         sd(UnrHead.Nrel$vtime)/ sqrt(length(UnrHead.Nrel$vtime)),
+         sd(UnrHead.NUnr$vtime)/ sqrt(length(UnrHead.NUnr$vtime))
   ))
-
-
-
-
 
 #
 # --------------------------------2 X 2 X 2 ANOVA-----------------------------------------------------
 
-sink("output/SemRel F1 Factor Analyses.txt")
+sink("output/Reprime Category F1 Factorial Analyses.txt")
 
 cat(" ", "\n")
-cat("BY-SUBJECTS FACTOR ANALYSES RUN ON:", format(Sys.time(), "%b. %d, %Y at %T"), sep = "", fill= 70)
+cat("BY-SUBJECTS FACTORIAL ANALYSES RUN ON:", format(Sys.time(), "%b. %d, %Y at %T"), sep = "", fill= 70)
 cat(" ", "\n")
 cat(rep(c("-"), times=40, quote=F),"\n")
-cat("2X2X2 ANOVA: SEMREL", sep = "", fill = 60)
+cat("2X2 ANOVA: CATEGORY COORDINATES", sep = "", fill = 60)
 cat(rep(c("-"), times=40, quote=F), "\n")
 print(ds) # prints descrip stats for 2x2x2 ANOVA
 cat(" ", "\n")
 
 # Computes the anova
-a.2x2x2 <- aov(error ~ semint * related * n2num + Error(subj / (semint * related * n2num)), data = data.subj)
-print(summary(a.2x2x2)) 
+a.2x2 <- aov(vtime ~ headrel * localrel + Error(subj / (headrel * localrel)), data = d.cat)
+print(summary(a.2x2)) 
 cat(" ", "\n")
 cat(" ", "\n")
 
@@ -248,101 +115,43 @@ cat(" ", "\n")
 #
 #========================================================================================================
 #
-#------------------------------------RELATED - UNRELATED ITEMS PAIRED COMPARISONS--------------------------------------
+#------------------------------------RELATED HEAD vs. UNRELATED HEADS-------------------------------------
 #
-
-f1errout <- read.table("data/SR_F1_errrel.txt", header = T) #reads in Related data, ignoring integration
-d <- f1errout 
-d$subj <- as.factor(d$subj)
-d$pct <- ifelse(d$errd  == 0 & d$errcord == 0, 0, (d$errd / (d$errcord)) * 100) 
-data.subj <- aggregate(d$pct, list(d$subj, d$semint, d$related, d$n2num ), mean) 
-colnames(data.subj) <- c("subj", "semint", "related", "n2num", "error") 
-
-relat      <- subset(data.subj, related == "rel") 
-unrel      <- subset(data.subj, related == "unrel") 
-sing       <- subset(data.subj, n2num   == "sing") 
-plur       <- subset(data.subj, n2num   == "plur")
-relat.plur <- subset(data.subj, related == "rel"   & n2num == "plur") 
-relat.sing <- subset(data.subj, related == "rel"   & n2num == "sing")
-unrel.plur <- subset(data.subj, related == "unrel" & n2num == "plur")
-unrel.sing <- subset(data.subj, related == "unrel" & n2num == "sing")
 
 ds <- data.frame(data = c(
   "gmean",
-  "relat",
-  "unrel",
-  "plur",
-  "sing",
-  "relatplur",
-  "relatsing",
-  "unrelplur",
-  "unrelsing"
-),
-
-n = c(length(data.subj$error),
-      length(relat$error),
-      length(unrel$error),
-      length(plur$error),
-      length(sing$error),
-      length(relat.plur$error),
-      length(relat.sing$error),
-      length(unrel.plur$error),
-      length(unrel.sing$error)
-),
-
-N = c(length(data.subj$error),
-      length(relat$error),
-      length(unrel$error),
-      length(plur$error),
-      length(sing$error),
-      length(relat.plur$error),
-      length(relat.sing$error),
-      length(unrel.plur$error),
-      length(unrel.sing$error)
-),
-
-mean = c(mean(data.subj$error),
-         mean(relat$error),
-         mean(unrel$error),
-         mean(plur$error),
-         mean(sing$error),
-         mean(relat.plur$error),
-         mean(relat.sing$error),
-         mean(unrel.plur$error),
-         mean(unrel.sing$error)
-),
-
-sd = c(sd(data.subj$error),
-       sd(relat$error),
-       sd(unrel$error),
-       sd(plur$error),
-       sd(sing$error),
-       sd(relat.plur$error),
-       sd(relat.sing$error),
-       sd(unrel.plur$error),
-       sd(unrel.sing$error)
-),
-
-se = c(sd(data.subj$error) / sqrt(length(data.subj$error)),
-       sd(relat$error) / sqrt(length(relat$error)),
-       sd(unrel$error) / sqrt(length(unrel$error)),
-       sd(plur$error) / sqrt(length(plur$error)),
-       sd(sing$error) / sqrt(length(sing$error)),
-       sd(relat.plur$error) / sqrt(length(relat.plur$error)),
-       sd(relat.sing$error) / sqrt(length(relat.sing$error)),
-       sd(unrel.plur$error) / sqrt(length(unrel.plur$error)),
-       sd(unrel.sing$error) / sqrt(length(unrel.sing$error))
-       
-))
+  "HeadN",
+  "UnrHead"),
+  
+  n = c(length(d.cat$vtime),
+        length(HeadN$vtime), 
+        length(UnrHead$vtime)),
+  
+  N = c(length(d.cat$vtime),
+        length(HeadN$vtime), 
+        length(UnrHead$vtime)),
+  
+  mean = c(mean(d.cat$vtime),
+           mean(HeadN$vtime), 
+           mean(UnrHead$vtime)),
+  
+  sd = c(sd(d.cat$vtime),
+         sd(HeadN$vtime), 
+         sd(UnrHead$vtime)),
+  
+  se = c(sd(d.cat$vtime)       / sqrt(length(d.cat$vtime)),
+         sd(HeadN$vtime)       / sqrt(length(HeadN$vtime)), 
+         sd(UnrHead$vtime)     / sqrt(length(UnrHead$vtime))
+  ))
 
 cat(rep(c("-"), times=40, quote=F),"\n")
-cat("RELATED ITEMS ANALYSES", sep = "", fill = 60)
+cat("RELATED HEAD vs. UNRELATED HEAD", sep = "", fill = 60)
 cat(rep(c("-"), times=40, quote=F), "\n")
 print(ds) 
 cat(" ", "\n")
 
-a.2x2 <- aov(error ~ related * n2num + Error(subj / (related * n2num)), data = data.subj) 
-print(summary(a.2x2)) 
+a.rhunrh <- aov(vtime ~ headrel * localrel + Error(subj / (headrel * localrel)), data = d.cat) 
+print(summary(a.rhunrh)) 
 cat(" ", "\n")
 cat(" ", "\n")
 
